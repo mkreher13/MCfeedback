564dfe61eb199722925ba8d6397725642d5a01fc
Miriam Rathbun <miriamrathbun13@gmail.com>
2018-01-25 13:57:50 -0500

    Latest working version with fuel temp
    This version includes an average fuel temperature calculation using constance thermal conductivity of the fuel. 
    It has been modified to be fully in Kelvin. 
    Note: the wall temperature (Tw) in this version is computed by converging on the entire channel. Upcoming versions will converge on Tw at each node.

--- thermal.py
+++ thermal.py
@@ -30,19 +30,12 @@ class Thermal():
 		Tin = options.Tin
 		G = options.G
 		L = options.length
-		muf = 6.829e-5  #[J/kg]
-		cpf = 8.976e3   #[J/kg/K]
-		k = 2.163       #[W/m/K] Fuel, should it be the cladding since that's what the water is in contact with? 
-		q = 17860       #[W/m]
-		rhof = 613      #[kg/m3]
-		rhog = 90.9     #[kg/m3]
-		sigma = 0.00563 #[N/m]
+		q = 17860.       #[W/m]
 
-		subcooled_liq = IAPWS97(T=Tin+273, x=0)
+		subcooled_liq = IAPWS97(T=Tin, x=0)
 		sat_liq = IAPWS97(P=P, x=0)
 		sat_steam = IAPWS97(P=P, x=1)
-		Tsat = sat_liq.T-273
-
+		Tsat = sat_liq.T
 
 		LinPower = np.zeros(self.zMesh)
 		HeatFlux = np.zeros(self.zMesh)
@@ -50,7 +43,7 @@ class Thermal():
 		self.enthalpy = np.zeros(self.zMesh)
 		self.Tbulk = np.zeros(self.zMesh)
 		self.Tw = np.zeros(self.zMesh)
-		self.HTC_nb = np.zeros(self.zMesh)
+		self.velocity = np.zeros(self.zMesh)
 		LinPower[:] = q
 		HeatFlux[:] = q/(2*np.pi*options.CladOR)
 		self.Tw[:] = Tin # Initial guess
@@ -61,8 +54,8 @@ class Thermal():
 		# And converging to Tw (wall temperature)
 
 
-		rho_in = IAPWS97(T=Tin+273, x=0).rho
-		E = 1000
+		rho_in = IAPWS97(T=Tin, x=0).rho
+		E = 1000.
 		count = 1
 
 		while E > 1E-3:
@@ -78,43 +71,45 @@ class Thermal():
 				self.enthalpy[i] = LinPower[i]/1000*L/self.zMesh/(G*A) + self.enthalpy[i-1]
 				self.Tbulk[i] = (self.enthalpy[i]-self.enthalpy[i-1])/(liq.cp) + self.Tbulk[i-1]
 
-				liq = IAPWS97(T=self.Tbulk[i]+273, x=0)
-				stm = IAPWS97(T=self.Tbulk[i]+273, x=1)
-				Tw_water = IAPWS97(T=self.Tw[i]+273, x=0)
+				liq = IAPWS97(T=self.Tbulk[i], x=0)
+				stm = IAPWS97(T=self.Tbulk[i], x=1)
+				Tw_water = IAPWS97(T=self.Tw[i], x=0)
 				Re = G*De/liq.mu
 				Pr = liq.cp*1000*liq.mu/liq.k
-				Nu = 0.0023*(Re**0.8)*(Pr**0.4)
-				HTC_c = 0.023*(G*De/liq.mu)**0.8*Pr**0.4*liq.k/De
+				Nu = 0.023*(Re**0.8)*(Pr**0.4)
+				HTC_c = Nu*liq.k/De
 				S = 1/(1+2.53E-6*Re**1.17)
-				self.HTC_nb[i] = S*0.00122*(liq.k**0.79)*((liq.cp*1000)**0.45)*(liq.rho**0.49)/(liq.sigma**0.5)/(liq.mu**0.29)/((stm.h-liq.h)**0.24)/(stm.rho**0.24)*(abs(self.Tw[i]-Tsat)**0.24)*(abs(Tw_water.P-sat_liq.P)**0.75)
+				HTC_nb = S*0.00122*(liq.k**0.79)*((liq.cp*1000)**0.45)*(liq.rho**0.49)/(liq.sigma**0.5)/(liq.mu**0.29)/((stm.h-liq.h)**0.24)/(stm.rho**0.24)*(abs(self.Tw[i]-Tsat)**0.24)*(abs(Tw_water.P-sat_liq.P)**0.75)
 
 
 				# Conservation equations
+				rho_top = IAPWS97(T=self.Tbulk[i], x=0).rho
+				rho_bot = IAPWS97(T=self.Tbulk[i-1], x=0).rho
 
-				# Energy
-				a = G*(self.enthalpy[i]-self.enthalpy[i-1])/(L/self.zMesh)
-				b = LinPower[i]/1000/A
+				# Mass
+				self.velocity[i] = G/rho_top
+
+				# Energy - used to calculate enthaply, so no need to check. 
+				#a = G*(self.enthalpy[i]-self.enthalpy[i-1])/(L/self.zMesh)
+				#b = LinPower[i]/1000/A
 				#print a-b
 
 				# Momentum 
-				rho_top = IAPWS97(T=self.Tbulk[i]+273, x=0).rho
-				rho_bot = IAPWS97(T=self.Tbulk[i-1]+273, x=0).rho
-				deltaP = G**2*(1/rho_top-1/rho_bot)
+				acc = G**2*(1/rho_top-1/rho_bot)
+				f = 0.184/527714.535424**0.2
+				fric = f*G**2/De/2/((rho_top+rho_bot)/2)*L/self.zMesh
+				grav = 9.81*(rho_top+rho_bot)/2*L/self.zMesh
+				deltaP = acc + fric + grav
 				P_sum = P_sum + deltaP
 				
 
-			rho_out = IAPWS97(T=self.Tbulk[self.zMesh-1]+273, x=0).rho
-			totalP = G**2*(1/rho_out-1/rho_in)#*L
-			#print("total pressure change with totalP:")
-			#print totalP/(10**6)
-			#print("total pressure change with deltaP:")
-			#print P_sum/(10**6)
-				
-				
+			rho_out = IAPWS97(T=self.Tbulk[self.zMesh-1], x=0).rho
+			totalP = G**2*(1/rho_out-1/rho_in) + f*G**2/De/2/((rho_out+rho_in)/2)*L + 9.81*(rho_out+rho_in)/2*L 
+			#print("Total pressure change between inlet/outlet: %f" % totalP)
+			#print("Total pressure change by sum of increments: %f" % P_sum)
+			#print("Velocity in the channel:")
 
-
-
-			self.Tw_updated = (HeatFlux[:]+self.Tbulk[:]*HTC_c+Tsat*self.HTC_nb[:])/(HTC_c+self.HTC_nb[:])
+			self.Tw_updated = (HeatFlux[:]+self.Tbulk[:]*HTC_c+Tsat*HTC_nb)/(HTC_c+HTC_nb)
 
 			E = max(abs(self.Tw_updated[:]-self.Tw[:]))
 
@@ -123,16 +118,32 @@ class Thermal():
 			self.Tw[:] = self.Tw_updated
 
 		
-		print("Temperature at the wall:")
-		print self.Tw
-		print ("Temperature in the bulk liquid:")
-		print self.Tbulk
+		print("Temperature in the bulk liquid [C]:")
+		print(self.Tbulk)
+		print("Temperature at the wall [C]:")
+		print(self.Tw)
+		# print("Water velocity [m/s]:")
+		# print(self.velocity)
+		# print("Total pressure change between inlet/outlet: %f [Pa]" % P_sum)
 
-			
 
+		################
+		# Fuel Temperature
 
+		self.Tf = np.zeros(self.zMesh)
+		kf = 2.4             #[W/m/K]
+		kc = 17.             #[W/m/K]
+		hg = 31000.          #[W/m^2/K]
+		clad_or = 0.00475    #[m]
+		clad_ir = 0.0040005  #[m]
+		fuel_or = 0.0039218  #[m]
+		gap_r = (clad_ir+fuel_or)/2 
 
+		for i in range(0,self.zMesh):
+			self.Tf[i] = self.Tw[i] + q/2.0/np.pi*(1.0/4.0/kf+1/gap_r/hg+1/kc*np.log(clad_or/clad_ir))
 
+		print("Temperature in the fuel [C]:")
+		print(self.Tf)
 
 
 
